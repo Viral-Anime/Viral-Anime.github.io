22a2989d391a6f37f40b7065b7efc7b058251ff8
Viral Anime <vk1371198@gmail.com>
2026-03-19 00:54:24 +0530

    Update data.js

--- data.js
+++ data.js
@@ -25,5 +25,34 @@ const animeList = [
             "https://vidmoly.net/embed-9zd0zlr2ky26.html"
         ],
         server2: []
+    },
+
+    {
+        id: "solo-leveling",
+        title: "Solo Leveling (Hindi Dubbed)",
+        poster: "https://image.tmdb.org/t/p/w500/geCRmSbt2dvGh3IhpbrvwwqMTky.jpg",
+        year: "2024",
+        duration: "24m",
+        rating: "8.7",
+        genres: "Action, Adventure, Fantasy",
+        cast: "Taito Ban, Reina Ueda",
+        description: "In a world where hunters must battle deadly monsters to protect mankind, Sung Jinwoo, a notoriously weak hunter, finds himself in a struggle for survival.",
+        server1: [
+            "https://animehindiworld.seekplays.com/#kcaxn", // Ep 1
+            "https://animehindiworld.seekplays.com/#ia65p", // Ep 2
+            "https://animehindiworld.seekplays.com/#3ubig", // Ep 3
+            "https://animehindiworld.seekplays.com/#ia65v", // Ep 4
+            "https://animehindiworld.seekplays.com/#wzdm6", // Ep 5
+            "https://animehindiworld.seekplays.com/#5t9ll", // Ep 6
+            "https://animehindiworld.seekplays.com/#ia6ij", // Ep 7
+            "https://animehindiworld.seekplays.com/#kcaky", // Ep 8
+            "https://animehindiworld.seekplays.com/#pwyov", // Ep 9
+            "https://animehindiworld.seekplays.com/#6evp5", // Ep 10
+            "https://animehindiworld.seekplays.com/#8ha56", // Ep 11
+            "https://animehindiworld.seekplays.com/#ajmbe"  // Ep 12
+        ],
+        server2: []
     }
+
+
 ];
