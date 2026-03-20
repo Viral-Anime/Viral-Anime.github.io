a82fc48fd3ec6c1d82f6d1c775f934b3975496f4
Viral Anime <vk1371198@gmail.com>
2026-03-20 13:55:18 +0530

    Update data.js

--- data.js
+++ data.js
@@ -62,5 +62,80 @@ const animeList = [
                 server2: []
             }
         ]
-    }
+    },
+{
+    "id": "haikyuu",
+    "title": "Haikyuu!!",
+    "poster": "https://image.tmdb.org/t/p/w92/placeholder.jpg",
+    "year": "2014",
+    "duration": "24m",
+    "rating": "8.7",
+    "genres": "Animation, Comedy, Drama, Sport",
+    "description": "Inspired after watching a volleyball ace nicknamed 'Little Giant' in action, small-statured Shouyou Hinata revives the volleyball club at his middle school. The newly-formed team even makes it to a tournament; however, their first match turns out to be their last when they are brutally squashed by the 'King of the Court,' Tobio Kageyama.",
+    "seasons": [
+        {
+            "name": "Season 1",
+            "server1": [
+                "https://animehindiworld.seekplays.com/#iuusx",
+                "https://animehindiworld.seekplays.com/#bwwgu",
+                "https://animehindiworld.seekplays.com/#nrrf6",
+                "https://animehindiworld.seekplays.com/#6mmot",
+                "https://animehindiworld.seekplays.com/#kss3x",
+                "https://animehindiworld.seekplays.com/#cbb9t",
+                "https://animehindiworld.seekplays.com/#o11wt",
+                "https://animehindiworld.seekplays.com/#kss38",
+                "https://animehindiworld.seekplays.com/#jvvma",
+                "https://animehindiworld.seekplays.com/#h66ax",
+                "https://animehindiworld.seekplays.com/#399tf",
+                "https://animehindiworld.seekplays.com/#h66aq",
+                "https://animehindiworld.seekplays.com/#wqqyw",
+                "https://animehindiworld.seekplays.com/#tjjby",
+                "https://animehindiworld.seekplays.com/#spp6p",
+                "https://animehindiworld.seekplays.com/#eff11",
+                "https://animehindiworld.seekplays.com/#5aasu",
+                "https://animehindiworld.seekplays.com/#vddqs",
+                "https://animehindiworld.seekplays.com/#lttc9",
+                "https://animehindiworld.seekplays.com/#mzzy8",
+                "https://animehindiworld.seekplays.com/#gyyvx",
+                "https://animehindiworld.seekplays.com/#uggjm",
+                "https://animehindiworld.seekplays.com/#aoop1",
+                "https://animehindiworld.seekplays.com/#8cc3h",
+                "https://animehindiworld.seekplays.com/#xhhi5"
+            ],
+            "server2": []
+        },
+        {
+            "name": "Season 2",
+            "server1": [
+                "https://animehindiworld.seekplays.com/#h66ur",
+                "https://animehindiworld.seekplays.com/#h66u8",
+                "https://animehindiworld.seekplays.com/#sppb9",
+                "https://animehindiworld.seekplays.com/#399az",
+                "https://animehindiworld.seekplays.com/#iuut8",
+                "https://animehindiworld.seekplays.com/#5aasw",
+                "https://animehindiworld.seekplays.com/#1xxr6",
+                "https://animehindiworld.seekplays.com/#xhh3i",
+                "https://animehindiworld.seekplays.com/#o11na",
+                "https://animehindiworld.seekplays.com/#nrrzb",
+                "https://animehindiworld.seekplays.com/#jvvwu",
+                "https://animehindiworld.seekplays.com/#kssh8",
+                "https://animehindiworld.seekplays.com/#iuut1",
+                "https://animehindiworld.seekplays.com/#gyy18",
+                "https://animehindiworld.seekplays.com/#o11vu",
+                "https://animehindiworld.seekplays.com/#9eeg1",
+                "https://animehindiworld.seekplays.com/#ltts6",
+                "https://animehindiworld.seekplays.com/#8cchc",
+                "https://animehindiworld.seekplays.com/#uggow",
+                "https://animehindiworld.seekplays.com/#399uq",
+                "https://animehindiworld.seekplays.com/#h669k",
+                "https://animehindiworld.seekplays.com/#iuuak",
+                "https://animehindiworld.seekplays.com/#8cchm",
+                "https://animehindiworld.seekplays.com/#5aat9",
+                "https://animehindiworld.seekplays.com/#sppbj"
+            ],
+            "server2": []
+        }
+    ]
+}
+
 ];
