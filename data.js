61829fc974c989ef8b767e27965c9de791d58903
Viral Anime <vk1371198@gmail.com>
2026-03-21 19:41:43 +0530

    Update data.js

--- data.js
+++ data.js
@@ -924,7 +924,55 @@ const animeList = [
             "server2": []
         }
     ]
+},
+{
+    "id": "kaiju-no-8",
+    "title": "Kaiju No. 8",
+    "poster": "https://image.tmdb.org/t/p/w92/placeholder.jpg",
+    "year": "2024",
+    "duration": "24m",
+    "rating": "8.4",
+    "genres": "Action, Sci-Fi, Animation",
+    "description": "In a world plagued by creatures known as Kaiju, Kafka Hibino aspires to enlist in the Japan Defense Force to defeat them. After a small Kaiju enters his body, he gains the ability to transform into one himself.",
+    "seasons": [
+        {
+            "name": "Season 1",
+            "server1": [
+                "https://animehindiworld.seekplays.com/#bwn81",
+                "https://animehindiworld.seekplays.com/#xhcop",
+                "https://animehindiworld.seekplays.com/#spjfz",
+                "https://animehindiworld.seekplays.com/#8csgg",
+                "https://animehindiworld.seekplays.com/#9emd5",
+                "https://animehindiworld.seekplays.com/#cbpyw",
+                "https://animehindiworld.seekplays.com/#5auwv",
+                "https://animehindiworld.seekplays.com/#kste9",
+                "https://animehindiworld.seekplays.com/#wqrcl",
+                "https://animehindiworld.seekplays.com/#uger8",
+                "https://animehindiworld.seekplays.com/#spjfv",
+                "https://animehindiworld.seekplays.com/#efz3i"
+            ],
+            "server2": []
+        },
+        {
+            "name": "Season 2",
+            "server1": [
+                "https://animehindiworld.seekplays.com/#9emxi",
+                "https://animehindiworld.seekplays.com/#vdxaw",
+                "https://animehindiworld.seekplays.com/#pnvlu",
+                "https://animehindiworld.seekplays.com/#1x89z",
+                "https://animehindiworld.seekplays.com/#5aunl",
+                "https://animehindiworld.seekplays.com/#396yt",
+                "https://animehindiworld.seekplays.com/#ltawy",
+                "https://animehindiworld.seekplays.com/#mzqc6",
+                "https://animehindiworld.seekplays.com/#5aun1",
+                "https://animehindiworld.seekplays.com/#iu91h",
+                "https://animehindiworld.seekplays.com/#aogr3"
+            ],
+            "server2": []
+        }
+    ]
 }
+
 
 
 
