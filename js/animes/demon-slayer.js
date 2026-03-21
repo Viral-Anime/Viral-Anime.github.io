afd404a6e3b43b040e3cd3243187ee8a3064497a
Viral Anime <vk1371198@gmail.com>
2026-03-21 23:05:24 +0530

    Create js/animes/demon-slayer.js

---
+++ js/animes/demon-slayer.js
@@ -0,0 +1,100 @@
+animeList.push({
+    id: "demon-slayer",
+    title: "Demon Slayer (Hindi Dub)",
+    poster: "https://image.tmdb.org/t/p/w500/85731.jpg",
+    year: "2019",
+    duration: "24m",
+    rating: "8.7",
+    genres: "Action, Fantasy, Adventure",
+    cast: "Natsuki Hanae, Akari Kito",
+    description: "Tanjiro Kamado sets out to become a demon slayer to find a cure for his sister Nezuko and avenge his family who were brutally murdered by a demon.",
+    seasons: [
+        {
+            name: "Season 1",
+            server1: [
+                "https://animehindiworld.seekplays.com/#6mwiz",
+                "https://animehindiworld.seekplays.com/#9em5s",
+                "https://animehindiworld.seekplays.com/#wqr9c",
+                "https://animehindiworld.seekplays.com/#ltayr",
+                "https://animehindiworld.seekplays.com/#h6bro",
+                "https://animehindiworld.seekplays.com/#cbpdf",
+                "https://animehindiworld.seekplays.com/#bwn3r",
+                "https://animehindiworld.seekplays.com/#vdxpf",
+                "https://animehindiworld.seekplays.com/#tjoku",
+                "https://animehindiworld.seekplays.com/#nrd6x",
+                "https://animehindiworld.seekplays.com/#1x8jx",
+                "https://animehindiworld.seekplays.com/#ugel3",
+                "https://animehindiworld.seekplays.com/#5auzc",
+                "https://animehindiworld.seekplays.com/#8cs13",
+                "https://animehindiworld.seekplays.com/#pnvhp",
+                "https://animehindiworld.seekplays.com/#mzquo",
+                "https://animehindiworld.seekplays.com/#xhcnm",
+                "https://animehindiworld.seekplays.com/#396r6",
+                "https://animehindiworld.seekplays.com/#5aufq",
+                "https://animehindiworld.seekplays.com/#gyftj",
+                "https://animehindiworld.seekplays.com/#kst1g",
+                "https://animehindiworld.seekplays.com/#396q9",
+                "https://animehindiworld.seekplays.com/#8csvi",
+                "https://animehindiworld.seekplays.com/#1x8pd",
+                "https://animehindiworld.seekplays.com/#h6brb",
+                "https://animehindiworld.seekplays.com/#jv1cq"
+            ],
+            server2: []
+        },
+        {
+            name: "Season 2",
+            server1: [
+                "https://animehindiworld.seekplays.com/#5auzr",
+                "https://animehindiworld.seekplays.com/#ltafd",
+                "https://animehindiworld.seekplays.com/#iu9ru",
+                "https://animehindiworld.seekplays.com/#spj8k",
+                "https://animehindiworld.seekplays.com/#tjol9",
+                "https://animehindiworld.seekplays.com/#1x8o8",
+                "https://animehindiworld.seekplays.com/#gyfao",
+                "https://animehindiworld.seekplays.com/#h6bxj",
+                "https://animehindiworld.seekplays.com/#396db",
+                "https://animehindiworld.seekplays.com/#xhcvw",
+                "https://animehindiworld.seekplays.com/#spj8r",
+                "https://animehindiworld.seekplays.com/#8cs11",
+                "https://animehindiworld.seekplays.com/#9emit",
+                "https://animehindiworld.seekplays.com/#uge5h",
+                "https://animehindiworld.seekplays.com/#8csyh",
+                "https://animehindiworld.seekplays.com/#cbpxz",
+                "https://animehindiworld.seekplays.com/#vdxjz",
+                "https://animehindiworld.seekplays.com/#aogla"
+            ],
+            server2: []
+        },
+        {
+            name: "Season 3",
+            server1: [
+                "https://animehindiworld.seekplays.com/#tjods",
+                "https://animehindiworld.seekplays.com/#9emkh",
+                "https://animehindiworld.seekplays.com/#aogxc",
+                "https://animehindiworld.seekplays.com/#iu9fs",
+                "https://animehindiworld.seekplays.com/#pnvi6",
+                "https://animehindiworld.seekplays.com/#8csn5",
+                "https://animehindiworld.seekplays.com/#wqra3",
+                "https://animehindiworld.seekplays.com/#gyfcb",
+                "https://animehindiworld.seekplays.com/#5auy3",
+                "https://animehindiworld.seekplays.com/#ltavz",
+                "https://animehindiworld.seekplays.com/#5au1f"
+            ],
+            server2: []
+        },
+        {
+            name: "Season 4",
+            server1: [
+                "https://animehindiworld.seekplays.com/#iu9fj",
+                "https://animehindiworld.seekplays.com/#jv1hz",
+                "https://animehindiworld.seekplays.com/#ugeki",
+                "https://animehindiworld.seekplays.com/#kstvo",
+                "https://animehindiworld.seekplays.com/#spjdx",
+                "https://animehindiworld.seekplays.com/#kstvj",
+                "https://animehindiworld.seekplays.com/#5auyz",
+                "https://animehindiworld.seekplays.com/#lta1q"
+            ],
+            server2: []
+        }
+    ]
+});
