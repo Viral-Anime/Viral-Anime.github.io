0e4740fbc1837ca5080484a880dfe0366ea93543
Viral Anime <vk1371198@gmail.com>
2026-03-21 20:53:46 +0530

    Create js/animes/wind-breaker.js

---
+++ js/animes/wind-breaker.js
@@ -0,0 +1,33 @@
+const animeList = [
+    {
+        id: "wind-breaker",
+        title: "Wind Breaker (Hindi Dub)",
+        poster: "https://image.tmdb.org/t/p/w500/f0CZzRXyqCc41NePDJSY23YQ2SS.jpg",
+        year: "2024",
+        duration: "24m",
+        rating: "7.72",
+        genres: "Action, Anime, School",
+        cast: "Koki Uchiyama, Nobunaga Shimazaki",
+        description: "Haruka Sakura wants nothing to do with weaklings—he's only interested in the strongest of the strong. He's just started at Furin High School...",
+        seasons: [
+            {
+                name: "Season 1",
+                server1: [
+                    "https://vidmoly.net/embed-otx0kv0uyysp.html",
+                    "https://vidmoly.net/embed-w3aof5xz648a.html",
+                    "https://vidmoly.net/embed-xbla97mxraw2.html",
+                    "https://vidmoly.net/embed-7xp0m9w2mdkl.html",
+                    "https://vidmoly.net/embed-twu8lb79askx.html",
+                    "https://vidmoly.net/embed-sben16btq6f6.html",
+                    "https://vidmoly.net/embed-moc619ek69l3.html",
+                    "https://vidmoly.net/embed-7izjyy8qzus5.html",
+                    "https://vidmoly.net/embed-zzz54lpwl95b.html",
+                    "https://vidmoly.net/embed-ifgpim3e87dx.html",
+                    "https://vidmoly.net/embed-f95f3jc15soc.html",
+                    "https://vidmoly.net/embed-kg3uzhpbusrn.html",
+                    "https://vidmoly.net/embed-9zd0zlr2ky26.html"
+                ],
+                server2: [] // Isme links nahi hain, toh button nahi dikhega
+            }
+        ]
+    },]
