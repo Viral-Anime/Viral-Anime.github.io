cfd7c2f9d25be68a2d3fd781e32d6a00044e9673
Viral Anime <vk1371198@gmail.com>
2026-03-22 09:11:43 +0530

    Create js/animes/naruto-shippuden.js

---
+++ js/animes/naruto-shippuden.js
@@ -0,0 +1,95 @@
+animeList.push({
+    id: "naruto-shippuden-part-1",
+    title: "Naruto Shippuden (Hindi Dub)",
+    poster: "https://image.tmdb.org/t/p/w500/kVneSTKi8SnuXBrO9P1S6gOCjUv.jpg",
+    year: "2007",
+    duration: "24m",
+    rating: "8.6",
+    genres: "Action, Adventure, Fantasy",
+    cast: "Junko Takeuchi, Noriaki Sugiyama",
+    description: "Naruto Uzumaki returns to the Hidden Leaf Village after years of training to face the Akatsuki and bring his friend Sasuke back home.",
+    seasons: [
+        {
+            name: "Season 1",
+            server1: ["https://animehindiworld.seekplays.com/#8calj","https://animehindiworld.seekplays.com/#ksugr","https://animehindiworld.seekplays.com/#o1zns","https://animehindiworld.seekplays.com/#efry3","https://animehindiworld.seekplays.com/#1xlra","https://animehindiworld.seekplays.com/#o1znc","https://animehindiworld.seekplays.com/#8ca3j","https://animehindiworld.seekplays.com/#9enom","https://animehindiworld.seekplays.com/#h6juf","https://animehindiworld.seekplays.com/#pnymz","https://animehindiworld.seekplays.com/#iubtw","https://animehindiworld.seekplays.com/#bw1et","https://animehindiworld.seekplays.com/#ksuhr","https://animehindiworld.seekplays.com/#h6jur","https://animehindiworld.seekplays.com/#5a6tt","https://animehindiworld.seekplays.com/#spgb6","https://animehindiworld.seekplays.com/#wqxfe","https://animehindiworld.seekplays.com/#cbo9h","https://animehindiworld.seekplays.com/#nr8zu","https://animehindiworld.seekplays.com/#5a6ss","https://animehindiworld.seekplays.com/#vdkq3","https://animehindiworld.seekplays.com/#xht3k","https://animehindiworld.seekplays.com/#6mvgc","https://animehindiworld.seekplays.com/#spg6s","https://animehindiworld.seekplays.com/#8cahi","https://animehindiworld.seekplays.com/#ugwjo","https://animehindiworld.seekplays.com/#gyqvq","https://animehindiworld.seekplays.com/#tjepv","https://animehindiworld.seekplays.com/#6mves","https://animehindiworld.seekplays.com/#1xldu","https://animehindiworld.seekplays.com/#pnywq","https://animehindiworld.seekplays.com/#lt9a9"],
+            server2: []
+        },
+        {
+            name: "Season 2",
+            server1: ["https://animehindiworld.seekplays.com/#xhthl","https://animehindiworld.seekplays.com/#spgju","https://animehindiworld.seekplays.com/#wqxzm","https://animehindiworld.seekplays.com/#8cash","https://animehindiworld.seekplays.com/#jvfnt","https://animehindiworld.seekplays.com/#aomov","https://animehindiworld.seekplays.com/#h6j9z","https://animehindiworld.seekplays.com/#ugwge","https://animehindiworld.seekplays.com/#h6j6q","https://animehindiworld.seekplays.com/#mzdq8","https://animehindiworld.seekplays.com/#spgjp","https://animehindiworld.seekplays.com/#9enmv","https://animehindiworld.seekplays.com/#5a6uu","https://animehindiworld.seekplays.com/#5a6te","https://animehindiworld.seekplays.com/#o1zvs","https://animehindiworld.seekplays.com/#ksus8","https://animehindiworld.seekplays.com/#vdk5t","https://animehindiworld.seekplays.com/#6mvhq"],
+            server2: []
+        },
+        {
+            name: "Season 3",
+            server1: ["https://animehindiworld.seekplays.com/#o1zy6","https://animehindiworld.seekplays.com/#bw1vm","https://animehindiworld.seekplays.com/#nr8qr","https://animehindiworld.seekplays.com/#efrfu","https://animehindiworld.seekplays.com/#jvf16","https://animehindiworld.seekplays.com/#39ps9","https://animehindiworld.seekplays.com/#xhtsq","https://animehindiworld.seekplays.com/#pnyvg","https://animehindiworld.seekplays.com/#cbop6","https://animehindiworld.seekplays.com/#8ca3r","https://animehindiworld.seekplays.com/#9en1w","https://animehindiworld.seekplays.com/#ksu5a","https://animehindiworld.seekplays.com/#vdkxr","https://animehindiworld.seekplays.com/#h6j6t","https://animehindiworld.seekplays.com/#1xl8z","https://animehindiworld.seekplays.com/#lt9s3","https://animehindiworld.seekplays.com/#efrf1","https://animehindiworld.seekplays.com/#wqxdm"],
+            server2: []
+        },
+        {
+            name: "Season 4",
+            server1: ["https://animehindiworld.seekplays.com/#spgom","https://animehindiworld.seekplays.com/#gyqf5","https://animehindiworld.seekplays.com/#o1zfc","https://animehindiworld.seekplays.com/#mzdrr","https://animehindiworld.seekplays.com/#39p6m","https://animehindiworld.seekplays.com/#iubae","https://animehindiworld.seekplays.com/#nr8qc","https://animehindiworld.seekplays.com/#ksutc","https://animehindiworld.seekplays.com/#tjog3","https://animehindiworld.seekplays.com/#lt9sh","https://animehindiworld.seekplays.com/#8ca33","https://animehindiworld.seekplays.com/#cbopa","https://animehindiworld.seekplays.com/#9en1o","https://animehindiworld.seekplays.com/#1xl8m","https://animehindiworld.seekplays.com/#ksut6","https://animehindiworld.seekplays.com/#efryp","https://animehindiworld.seekplays.com/#vdkx8"],
+            server2: []
+        },
+        {
+            name: "Season 5",
+            server1: ["https://animehindiworld.seekplays.com/#h6jbp","https://animehindiworld.seekplays.com/#jvf1v","https://animehindiworld.seekplays.com/#bw1m8","https://animehindiworld.seekplays.com/#39p6r","https://animehindiworld.seekplays.com/#spgog","https://animehindiworld.seekplays.com/#gyqfn","https://animehindiworld.seekplays.com/#o1zf6","https://animehindiworld.seekplays.com/#mzdr5"],
+            server2: []
+        },
+        {
+            name: "Season 6",
+            server1: ["https://animehindiworld.seekplays.com/#bw1mw","https://animehindiworld.seekplays.com/#nr8qa","https://animehindiworld.seekplays.com/#efrfz","https://animehindiworld.seekplays.com/#jvf1j","https://animehindiworld.seekplays.com/#39psw","https://animehindiworld.seekplays.com/#xhtsc","https://animehindiworld.seekplays.com/#pnyv6","https://animehindiworld.seekplays.com/#cbopj","https://animehindiworld.seekplays.com/#8ca3e","https://animehindiworld.seekplays.com/#9en19","https://animehindiworld.seekplays.com/#ksu56","https://animehindiworld.seekplays.com/#vdkx5","https://animehindiworld.seekplays.com/#h6j68","https://animehindiworld.seekplays.com/#1xl8d","https://animehindiworld.seekplays.com/#lt9s6","https://animehindiworld.seekplays.com/#efrf9","https://animehindiworld.seekplays.com/#wqxds","https://animehindiworld.seekplays.com/#spgoa","https://animehindiworld.seekplays.com/#gyqfv","https://animehindiworld.seekplays.com/#o1zf1","https://animehindiworld.seekplays.com/#mzdr8","https://animehindiworld.seekplays.com/#39p65","https://animehindiworld.seekplays.com/#iubas","https://animehindiworld.seekplays.com/#nr8q5","https://animehindiworld.seekplays.com/#ksutj","https://animehindiworld.seekplays.com/#tjogm","https://animehindiworld.seekplays.com/#lt9sm","https://animehindiworld.seekplays.com/#8ca3j","https://animehindiworld.seekplays.com/#cbopt","https://animehindiworld.seekplays.com/#9en1p","https://animehindiworld.seekplays.com/#1xl8j"],
+            server2: []
+        },
+        {
+            name: "Season 7",
+            server1: ["https://animehindiworld.seekplays.com/#vdkyl","https://animehindiworld.seekplays.com/#8cal6","https://animehindiworld.seekplays.com/#mzdnf","https://animehindiworld.seekplays.com/#aom9g","https://animehindiworld.seekplays.com/#tjeue","https://animehindiworld.seekplays.com/#iubhg","https://animehindiworld.seekplays.com/#5a63p","https://animehindiworld.seekplays.com/#ugw6b"],
+            server2: []
+        },
+        {
+            name: "Season 8",
+            server1: ["https://animehindiworld.seekplays.com/#mzdyr","https://animehindiworld.seekplays.com/#mzdfd","https://animehindiworld.seekplays.com/#jvfws","https://animehindiworld.seekplays.com/#39pa1","https://animehindiworld.seekplays.com/#iuban","https://animehindiworld.seekplays.com/#8cac3","https://animehindiworld.seekplays.com/#ksucd","https://animehindiworld.seekplays.com/#gyq1r","https://animehindiworld.seekplays.com/#spgpa","https://animehindiworld.seekplays.com/#lt9ct","https://animehindiworld.seekplays.com/#h6j9d","https://animehindiworld.seekplays.com/#vdkrh","https://animehindiworld.seekplays.com/#efryn","https://animehindiworld.seekplays.com/#ksuhd","https://animehindiworld.seekplays.com/#39puy","https://animehindiworld.seekplays.com/#aompw","https://animehindiworld.seekplays.com/#1xlx9","https://animehindiworld.seekplays.com/#lt9sa","https://animehindiworld.seekplays.com/#wqxqw","https://animehindiworld.seekplays.com/#9engw","https://animehindiworld.seekplays.com/#ugwog","https://animehindiworld.seekplays.com/#tjebn","https://animehindiworld.seekplays.com/#bw1wu","https://animehindiworld.seekplays.com/#aomjn"],
+            server2: []
+        },
+        {
+            name: "Season 9",
+            server1: ["https://animehindiworld.seekplays.com/#gyqwf","https://animehindiworld.seekplays.com/#8ca5b","https://animehindiworld.seekplays.com/#9enpg","https://animehindiworld.seekplays.com/#5a6hj","https://animehindiworld.seekplays.com/#ksu5z","https://animehindiworld.seekplays.com/#mzdvz","https://animehindiworld.seekplays.com/#lt93c","https://animehindiworld.seekplays.com/#ksu5f","https://animehindiworld.seekplays.com/#1xlzs","https://animehindiworld.seekplays.com/#pnygy","https://animehindiworld.seekplays.com/#spgua","https://animehindiworld.seekplays.com/#wqxvj","https://animehindiworld.seekplays.com/#5a6hh","https://animehindiworld.seekplays.com/#39psn","https://animehindiworld.seekplays.com/#tje9m","https://animehindiworld.seekplays.com/#ugwbp","https://animehindiworld.seekplays.com/#vdkf5","https://animehindiworld.seekplays.com/#h6jt1","https://animehindiworld.seekplays.com/#nr8yt","https://animehindiworld.seekplays.com/#8cal8","https://animehindiworld.seekplays.com/#spga3"],
+            server2: []
+        },
+        {
+            name: "Season 10",
+            server1: ["https://animehindiworld.seekplays.com/#5a6sg","https://animehindiworld.seekplays.com/#iubtr","https://animehindiworld.seekplays.com/#1xlra","https://animehindiworld.seekplays.com/#o1znc","https://animehindiworld.seekplays.com/#8ca3j","https://animehindiworld.seekplays.com/#9enom","https://animehindiworld.seekplays.com/#h6juf","https://animehindiworld.seekplays.com/#pnymz","https://animehindiworld.seekplays.com/#iubtw","https://animehindiworld.seekplays.com/#bw1et","https://animehindiworld.seekplays.com/#ksuhr","https://animehindiworld.seekplays.com/#h6jur","https://animehindiworld.seekplays.com/#5a6tt","https://animehindiworld.seekplays.com/#spgb6","https://animehindiworld.seekplays.com/#wqxfe","https://animehindiworld.seekplays.com/#cbo9h","https://animehindiworld.seekplays.com/#nr8zu","https://animehindiworld.seekplays.com/#5a6ss","https://animehindiworld.seekplays.com/#vdkq3","https://animehindiworld.seekplays.com/#xht3k","https://animehindiworld.seekplays.com/#6mvgc","https://animehindiworld.seekplays.com/#spg6s","https://animehindiworld.seekplays.com/#8cahi","https://animehindiworld.seekplays.com/#ugwjo","https://animehindiworld.seekplays.com/#gyqvq"],
+            server2: []
+        },
+        {
+            name: "Season 11",
+            server1: ["https://animehindiworld.seekplays.com/#tjepv","https://animehindiworld.seekplays.com/#6mves","https://animehindiworld.seekplays.com/#1xldu","https://animehindiworld.seekplays.com/#pnywq","https://animehindiworld.seekplays.com/#lt9a9","https://animehindiworld.seekplays.com/#xhthl","https://animehindiworld.seekplays.com/#spgju","https://animehindiworld.seekplays.com/#wqxzm","https://animehindiworld.seekplays.com/#8cash","https://animehindiworld.seekplays.com/#jvfnt","https://animehindiworld.seekplays.com/#aomov","https://animehindiworld.seekplays.com/#h6j9z","https://animehindiworld.seekplays.com/#ugwge","https://animehindiworld.seekplays.com/#h6j6q","https://animehindiworld.seekplays.com/#mzdq8","https://animehindiworld.seekplays.com/#spgjp","https://animehindiworld.seekplays.com/#9enmv","https://animehindiworld.seekplays.com/#5a6uu","https://animehindiworld.seekplays.com/#5a6te","https://animehindiworld.seekplays.com/#o1zvs","https://animehindiworld.seekplays.com/#ksus8"],
+            server2: []
+        },
+        {
+            name: "Season 12",
+            server1: ["https://animehindiworld.seekplays.com/#5a6aa","https://animehindiworld.seekplays.com/#efrz1","https://animehindiworld.seekplays.com/#nr8q9","https://animehindiworld.seekplays.com/#bw1ma","https://animehindiworld.seekplays.com/#6mvmt","https://animehindiworld.seekplays.com/#gyqyd","https://animehindiworld.seekplays.com/#ksucx","https://animehindiworld.seekplays.com/#cbo6c","https://animehindiworld.seekplays.com/#iubad","https://animehindiworld.seekplays.com/#cbopt","https://animehindiworld.seekplays.com/#efrfv","https://animehindiworld.seekplays.com/#9enen","https://animehindiworld.seekplays.com/#xhtc5","https://animehindiworld.seekplays.com/#h6j6x","https://animehindiworld.seekplays.com/#ksusx","https://animehindiworld.seekplays.com/#8cacg","https://animehindiworld.seekplays.com/#efrqy","https://animehindiworld.seekplays.com/#tjeoy","https://animehindiworld.seekplays.com/#xhtsi","https://animehindiworld.seekplays.com/#39p6z","https://animehindiworld.seekplays.com/#o1zya","https://animehindiworld.seekplays.com/#1xl86","https://animehindiworld.seekplays.com/#jvf1u","https://animehindiworld.seekplays.com/#vdkxs","https://animehindiworld.seekplays.com/#5a6uw","https://animehindiworld.seekplays.com/#pnyvd","https://animehindiworld.seekplays.com/#8case","https://animehindiworld.seekplays.com/#ksutk","https://animehindiworld.seekplays.com/#cboja","https://animehindiworld.seekplays.com/#h6jbr","https://animehindiworld.seekplays.com/#wqxdv","https://animehindiworld.seekplays.com/#spgoj","https://animehindiworld.seekplays.com/#gyqfx"],
+            server2: []
+        },
+        {
+            name: "Season 13",
+            server1: ["https://animehindiworld.seekplays.com/#tjegf","https://animehindiworld.seekplays.com/#9enw1","https://animehindiworld.seekplays.com/#8catc","https://animehindiworld.seekplays.com/#5a699","https://animehindiworld.seekplays.com/#spgo9","https://animehindiworld.seekplays.com/#gyqz8","https://animehindiworld.seekplays.com/#o1zfu","https://animehindiworld.seekplays.com/#iub91","https://animehindiworld.seekplays.com/#ksut8","https://animehindiworld.seekplays.com/#h6jpk","https://animehindiworld.seekplays.com/#39pbq","https://animehindiworld.seekplays.com/#vdk8t","https://animehindiworld.seekplays.com/#nr8db","https://animehindiworld.seekplays.com/#bw1n9","https://animehindiworld.seekplays.com/#ugwmw","https://animehindiworld.seekplays.com/#wqxx1","https://animehindiworld.seekplays.com/#iub98","https://animehindiworld.seekplays.com/#h6jb8","https://animehindiworld.seekplays.com/#xhtt3","https://animehindiworld.seekplays.com/#bw1v6"],
+            server2: []
+        },
+        {
+            name: "Season 14",
+            server1: ["https://animehindiworld.seekplays.com/#o1zz9","https://animehindiworld.seekplays.com/#gyqqk","https://animehindiworld.seekplays.com/#1xlkb","https://animehindiworld.seekplays.com/#spggo","https://animehindiworld.seekplays.com/#h6jpd","https://animehindiworld.seekplays.com/#efrrf","https://animehindiworld.seekplays.com/#lt99b","https://animehindiworld.seekplays.com/#tjeez","https://animehindiworld.seekplays.com/#iub6k","https://animehindiworld.seekplays.com/#iub6y","https://animehindiworld.seekplays.com/#pny1x","https://animehindiworld.seekplays.com/#h6jjx","https://animehindiworld.seekplays.com/#9enny","https://animehindiworld.seekplays.com/#8caaw","https://animehindiworld.seekplays.com/#vdkka","https://animehindiworld.seekplays.com/#ksuu5","https://animehindiworld.seekplays.com/#8caas","https://animehindiworld.seekplays.com/#spgg6","https://animehindiworld.seekplays.com/#5a69n","https://animehindiworld.seekplays.com/#ugwwn","https://animehindiworld.seekplays.com/#iubbf","https://animehindiworld.seekplays.com/#39ppr","https://animehindiworld.seekplays.com/#mzddl","https://animehindiworld.seekplays.com/#pnyy8","https://animehindiworld.seekplays.com/#ksuak"],
+            server2: []
+        },
+        {
+            name: "Season 15",
+            server1: ["https://animehindiworld.seekplays.com/#5a6bb","https://animehindiworld.seekplays.com/#1xllp","https://animehindiworld.seekplays.com/#h6jjl","https://animehindiworld.seekplays.com/#lt96p","https://animehindiworld.seekplays.com/#8caun","https://animehindiworld.seekplays.com/#cbog9","https://animehindiworld.seekplays.com/#efrdz","https://animehindiworld.seekplays.com/#nr8ko","https://animehindiworld.seekplays.com/#jvff6","https://animehindiworld.seekplays.com/#ugwnv","https://animehindiworld.seekplays.com/#aomwz","https://animehindiworld.seekplays.com/#nr88j","https://animehindiworld.seekplays.com/#8caut","https://animehindiworld.seekplays.com/#pnyfk","https://animehindiworld.seekplays.com/#iubbl","https://animehindiworld.seekplays.com/#vdklu","https://animehindiworld.seekplays.com/#39pjd","https://animehindiworld.seekplays.com/#ksu95","https://animehindiworld.seekplays.com/#iubpz","https://animehindiworld.seekplays.com/#wqx8y","https://animehindiworld.seekplays.com/#6mvv9","https://animehindiworld.seekplays.com/#bw11b","https://animehindiworld.seekplays.com/#1xl5j","https://animehindiworld.seekplays.com/#spgeb","https://animehindiworld.seekplays.com/#spgeg","https://animehindiworld.seekplays.com/#h6jo8","https://animehindiworld.seekplays.com/#mzdx5","https://animehindiworld.seekplays.com/#5a66v"],
+            server2: []
+        },
+        {
+            name: "Season 16",
+            server1: ["https://animehindiworld.seekplays.com/#spgme","https://animehindiworld.seekplays.com/#ksu63","https://animehindiworld.seekplays.com/#9en1z","https://animehindiworld.seekplays.com/#8ca9a","https://animehindiworld.seekplays.com/#cboe6","https://animehindiworld.seekplays.com/#pnyzl","https://animehindiworld.seekplays.com/#5a6pp","https://animehindiworld.seekplays.com/#lt9bj","https://animehindiworld.seekplays.com/#aomnq","https://animehindiworld.seekplays.com/#vdk59","https://animehindiworld.seekplays.com/#efrxq","https://animehindiworld.seekplays.com/#tjewr"],
+            server2: []
+        }
+    ]
+});
+
+
