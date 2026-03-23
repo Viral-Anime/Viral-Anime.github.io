a8261bd64dd3d023f3cef1c901a76ab354a3ea40
Viral Anime <vk1371198@gmail.com>
2026-03-23 11:06:11 +0530

    Update kaguya-sama.js

--- js/animes/kaguya-sama.js
+++ js/animes/kaguya-sama.js
@@ -1,17 +1,16 @@
 animeList.push({
-    id: "kaguya-sama",
-    title: "Kaguya-sama: Love Is War (Hindi Dub)",
-    poster: "https://image.tmdb.org/t/p/w500/589777.jpg",
-    year: "2019",
-    duration: "24m",
-    rating: "8.5",
-    genres: "Comedy, Romance, Psychological, School",
-    cast: "Aoi Koga, Makoto Furukawa",
-    description: "Two genius students, Miyuki Shirogane and Kaguya Shinomiya, are in love with each other but are too proud to confess it first, leading to a series of psychological battles to make the other person confess.",
-    seasons: [
+        "id": "kaguya-sama-love-is-war",
+    "title": "Kaguya-sama: Love Is War",
+    "poster": "https://image.tmdb.org/t/p/w92/placeholder.jpg",
+    "year": "2019",
+    "duration": "24m",
+    "rating": "8.5",
+    "genres": "Comedy, Romance, Psychological",
+    "description": "Two genius students, Miyuki Shirogane and Kaguya Shinomiya, are in love with each other but are too proud to confess it first, leading to a series of psychological battles to make the other person confess.",
+    "seasons": [
         {
-            name: "Season 1",
-            server1: [
+            "name": "Season 1",
+            "server1": [
                 "https://animehindiworld.seekplays.com/#h6by6",
                 "https://animehindiworld.seekplays.com/#h6bya",
                 "https://animehindiworld.seekplays.com/#nrdtz",
@@ -25,7 +24,7 @@ animeList.push({
                 "https://animehindiworld.seekplays.com/#iu9v6",
                 "https://animehindiworld.seekplays.com/#kstm6"
             ],
-            server2: []
+            "server2": []
         }
     ]
 });
