641646e6cf3146d4962b224fb26aa1177e04826c
Viral Anime <vk1371198@gmail.com>
2026-03-23 10:46:37 +0530

    Update solo-leveling.js

--- js/animes/solo-leveling.js
+++ js/animes/solo-leveling.js
@@ -1,31 +1,33 @@
-animeList.push({
-    id: "solo-leveling",
-    title: "Solo Leveling (Hindi Dub)",
-    poster: "https://image.tmdb.org/t/p/w500/ge68tY1CU9du9Qq4pT6o9adEFvI.jpg",
-    year: "2024",
-    duration: "24m",
-    rating: "8.7",
-    genres: "Action, Adventure, Fantasy",
-    cast: "Taito Ban, Genta Nakamura",
-    description: "In a world where hunters must battle deadly monsters to protect mankind, Sung Jinwoo, the weakest hunter of all, finds himself in a double dungeon and receives a mysterious system that allows him to level up alone.",
-    seasons: [
-        {
-            name: "Season 1",
-            server1: [
-                "https://animehindiworld.seekplays.com/#o1yvx",
-                "https://animehindiworld.seekplays.com/#1x8uj",
-                "https://animehindiworld.seekplays.com/#wqrs9",
-                "https://animehindiworld.seekplays.com/#iu9vj",
-                "https://animehindiworld.seekplays.com/#kst6v",
-                "https://animehindiworld.seekplays.com/#6mwzv",
-                "https://animehindiworld.seekplays.com/#bwnky",
-                "https://animehindiworld.seekplays.com/#o1yiv",
-                "https://animehindiworld.seekplays.com/#1x8uf",
-                "https://animehindiworld.seekplays.com/#wqrs5",
-                "https://animehindiworld.seekplays.com/#iu9v6",
-                "https://animehindiworld.seekplays.com/#kstm6"
-            ],
-            server2: []
-        }
-    ]
+animeList.push(
+    {
+        id: "solo-leveling",
+        title: "Solo Leveling (Hindi Dubbed)",
+        poster: "https://image.tmdb.org/t/p/w500/rsOApVLbIQEcNkqSlOxNPyg3FyI.jpg",
+        year: "2024",
+        duration: "24m",
+        rating: "8.7",
+        genres: "Action, Adventure, Fantasy",
+        cast: "Taito Ban, Reina Ueda",
+        description: "In a world where hunters must battle deadly monsters to protect mankind, Sung Jinwoo, a notoriously weak hunter, finds himself in a struggle for survival.",
+        seasons: [
+            {
+                name: "Season 1",
+                server1: [
+                    "https://animehindiworld.seekplays.com/#kcaxn",
+                    "https://animehindiworld.seekplays.com/#ia65p",
+                    "https://animehindiworld.seekplays.com/#3ubig",
+                    "https://animehindiworld.seekplays.com/#ia65v",
+                    "https://animehindiworld.seekplays.com/#wzdm6",
+                    "https://animehindiworld.seekplays.com/#5t9ll",
+                    "https://animehindiworld.seekplays.com/#ia6ij",
+                    "https://animehindiworld.seekplays.com/#kcaky",
+                    "https://animehindiworld.seekplays.com/#pwyov",
+                    "https://animehindiworld.seekplays.com/#6evp5",
+                    "https://animehindiworld.seekplays.com/#8ha56",
+                    "https://animehindiworld.seekplays.com/#ajmbe"
+                ],
+                server2: []
+            }
+        ]
+    
 });
